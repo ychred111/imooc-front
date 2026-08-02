1667d9d8fa0e4c26248854732586d2af9d9e4b35
ychred111 <ych13100574157@gmail.com>
2026-08-02 16:12:45 +0800

    feat(data): data-数据获取流程简化

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,23 @@
+import { promiseTimeout } from '@vueuse/core'
 import axios from 'axios'
 const service = axios.create({
   baseURL: import.meta.env.VITE_BASE_API,
   timeout: 5000
+})
+
+// 添加请求拦截器
+// service.interceptors.request.use()
+
+// 添加响应拦截器
+service.interceptors.response.use((response) => {
+  const { success, data, message } = response.data
+  // 要根据 success 的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // 业务错误
+    return Promise.reject(new Error(message))
+  }
 })
 
 export default service
