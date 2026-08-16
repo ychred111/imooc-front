9495f5d52bd07de74a3103777da7f9c7f7dd9ca4
ychred111 <ych13100574157@gmail.com>
2026-08-16 18:17:18 +0800

    feat(navgationbar-pc): navgationbar-pc端私有视图处理

--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -4,23 +4,27 @@ import { ALL_CATEGORY_ITEM } from '@/constants/index.js'
 // 处理navigation bar 里面的数据 category
 export default {
   //  标记独立作用域
-  namespace: true,
+  namespaced: true,
   // 为了保证每个使用该模块的组件都拥有独立且隔离的数据副本，防止多个组件共享同一个对象引用导致数据互相污染
-  state: () => {
-    // navigation 展示的数据源
-    categorys: [ALL_CATEGORY_ITEM]
-  },
+  state: () => ({
+    // navigationBar 展示的数据源
+    category: [ALL_CATEGORY_ITEM]
+  }),
   mutations: {
-    //   为 categorys 赋值
-    setCaetgorys(state, categorys) {
+    /**
+     * 为 categories 赋值
+     */
+    setCategorys(state, categorys) {
       state.categorys = [ALL_CATEGORY_ITEM, ...categorys]
     }
   },
   actions: {
-    //   获取 categorys 数据，并自动保存到vuex里面
-    async useCaetgorysData(context) {
+    /**
+     * 获取 category 数据，并自动保存到 vuex 中
+     */
+    async useCategoryData(context) {
       const { categorys } = await getCategory()
-      context.commit('setCaetgorys', categorys)
+      context.commit('setCategorys', categorys)
     }
   }
 }
--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,12 +1,14 @@
 <template>
   <mobile-navigation-vue v-if="isMobileTerminal" />
+  <pc-navigation-vue v-else />
 </template>
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
 import mobileNavigationVue from './mobile/index.vue'
+import pcNavigationVue from './pc/index.vue'
 import { useStore } from 'vuex'
 
 const store = useStore()
 // 触发 category 获取数据
-store.dispatch('category/useCaetgorysData')
+store.dispatch('category/useCategoryData')
 </script>
