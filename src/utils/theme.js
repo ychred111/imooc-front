6295ff92d6a7fe6c00104d74a28120981bb614d3
ychred111 <ych13100574157@gmail.com>
2026-08-18 14:56:34 +0800

    feat(theme): theme:跟随系统切换主题

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,23 +2,38 @@ import { watch } from 'vue'
 import store from '@/store/index.js'
 import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants'
 
+// 监听系统主题的变更
+let matchMedia
+const watchSystemThemeChange = () => {
+  // 仅需初始化一次即可
+  if (matchMedia) return
+  matchMedia = window.matchMedia('(prefers-color-scheme:dark)')
+  // 监听主题变化
+  matchMedia.onchange = function () {
+    changeTheme(THEME_SYSTEM)
+  }
+}
+
+// 变更主题函数抽离
+const changeTheme = (theme) => {
+  // 定义 html 的 class
+  let themeClassName = ''
+  // 修改 html class
+  switch (theme) {
+    case THEME_LIGHT:
+      themeClassName = 'light'
+      break
+    case THEME_DARK:
+      themeClassName = 'dark'
+      break
+    case THEME_SYSTEM:
+      themeClassName = matchMedia.matches ? 'dark' : 'light'
+      break
+  }
+  document.querySelector('html').className = themeClassName
+}
+// 初始化主题
 export default () => {
-  watch(
-    () => store.getters.themeType,
-    (val) => {
-      // 定义 html 的 class
-      let themeClassName = ''
-      // 修改 html class
-      switch (val) {
-        case THEME_LIGHT:
-          themeClassName = 'light'
-          break
-        case THEME_DARK:
-          themeClassName = 'dark'
-          break
-      }
-      document.querySelector('html').className = themeClassName
-    },
-    { immediate: true }
-  )
+  watchSystemThemeChange()
+  watch(() => store.getters.themeType, changeTheme, { immediate: true })
 }
--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full bg-white border-b border-b-zinc-200 dark:bg-zinc-900 dark:border-b-zinc-900 px-2 py-1"
+    class="w-full bg-white border-b border-b-zinc-200 dark:bg-zinc-900 dark:border-b-zinc-900 px-2 py-1 duration-500"
   >
     <div class="flex items-center">
       <img
